282f9bd04741c719f5c6ce29710a2f878dfaa739
karthikeyan <karthikeyankes@gmail.com>
2025-02-16 20:57:58 +0530

    with different properties file

--- demo/src/main/java/com/sampleworld/DemoApplication.java
+++ demo/src/main/java/com/sampleworld/DemoApplication.java
@@ -9,6 +9,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "sample")
+@Configuration("application")
+
 @SpringBootApplication
 public class DemoApplication {
 
